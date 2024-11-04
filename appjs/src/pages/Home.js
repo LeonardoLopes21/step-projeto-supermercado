3ee063f228935f9383ea43afc31005d4247b1095
EduardaGranville <eduardagranvilleleal@gmail.com>
2024-11-04 20:38:16 -0300

    Create Home.js

---
+++ appjs/src/pages/Home.js
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+      <div className="page">
+        <h1>Bem-vindo à Home</h1>
+        <p>Página inicial.</p>
+        {/* colocar alguma igmagem de supermercado ou alguma historia */}
+      </div>
+    );
+  }
+  
+  export default Home;
