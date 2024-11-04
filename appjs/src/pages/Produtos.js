ef6a7fb1afb53b96c6a6125263db2c2fee9481cc
EduardaGranville <eduardagranvilleleal@gmail.com>
2024-11-04 20:37:23 -0300

    Create Produtos.js

---
+++ appjs/src/pages/Produtos.js
@@ -0,0 +1,10 @@
+function Produtos() {
+    return (
+      <div className="page">
+        <h1>Nossos Produtos</h1>
+        <p>Lista de produtos.</p>
+      </div>
+    );
+  }
+  
+  export default Produtos;
