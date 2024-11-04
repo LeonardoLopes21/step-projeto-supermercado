0c88c33fb44614b8f5c5b0fe322c500a0e15d8f8
EduardaGranville <eduardagranvilleleal@gmail.com>
2024-11-04 20:37:51 -0300

    Create Carrinho.js

---
+++ appjs/src/pages/Carrinho.js
@@ -0,0 +1,10 @@
+function Carrinho() {
+    return (
+      <div className="page">
+        <h1>Seu Carrinho</h1>
+        <p>Itens no carrinho.</p>
+      </div>
+    );
+  }
+  
+  export default Carrinho;
